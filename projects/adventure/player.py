acf888b4a91a92d97e9292ddc3e1d5b76f72701d
Jasmine <jasminecharles@hotmail.com>
2020-07-28 23:16:10 -0700

    changes

--- projects/adventure/player.py
+++ projects/adventure/player.py
@@ -1,6 +1,10 @@
+from util import Queue, Stack
+from graph import Graph
+
 class Player:
     def __init__(self, starting_room):
         self.current_room = starting_room
+        self.vertice_rooms = {}
     def travel(self, direction, show_rooms = False):
         next_room = self.current_room.get_room_in_direction(direction)
         if next_room is not None:
@@ -9,3 +13,5 @@ class Player:
                 next_room.print_room_description(self)
         else:
             print("You cannot move in that direction.")
+
+     
